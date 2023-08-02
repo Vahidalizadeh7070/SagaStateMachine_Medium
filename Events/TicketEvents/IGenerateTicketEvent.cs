69dcc165256b7ff94a61ab3a7af340bc402a45b8
vahidalizadeh7070 <vahidalizadeh7070@gmail.com>
2023-08-03 00:20:44 +0330

    Add Events to the Events class library

---
+++ Events/TicketEvents/IGenerateTicketEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events.TicketEvents
+{
+    public interface IGenerateTicketEvent
+    {
+        public Guid TicketId { get; }
+        public string Title { get; }
+        public string Email { get; }
+        public DateTime RequireDate { get; }
+        public int Age { get; }
+        public string Location { get; }
+        public string TicketNumber { get; }
+    }
+}
